4c4daeaf044c7c5d614ef2d1f6e36a5a7cd3c973
JagadishGunnanti <root@ip-172-31-43-143.us-east-2.compute.internal>
2018-11-11 06:59:22 +0000

    Latest Commit

---
+++ UserSearch_API_Python.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import requests
+import json
+import sys
+from requests.auth import HTTPBasicAuth
+import getpass
+pwd = getpass.getpass('Password:')
+dynamicurl='https://api.github.com/users'
+response=requests.get(dynamicurl,auth=HTTPBasicAuth(sys.argv[1],pwd))
+print(response)
+obj=response.json()
+while True:
+ for i in range(0,len(obj)):
+  with open('output.json', 'a') as f:
+   output="loginID:"+str(obj[i]["id"])
+   print( output, file=f)
+   link=response.headers.get('link',None)
+ if link is not None:
+       url = link
+       dynamicurl=url[1:].split(">")[0]
+       print (dynamicurl)
+       response=requests.get(dynamicurl,auth=HTTPBasicAuth(sys.argv[1],pwd))
+       obj=response.json()
+ else:
+      print("I am done with searching!!")
+      break
+
